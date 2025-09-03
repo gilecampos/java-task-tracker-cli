89d103a01301946a206f7b0ce0306a44664d7580
Gileandro Campos <gileandrocampos8@gmail.com>
2025-09-03 11:43:41 -0300

    feat(structure): add commands sctruture to CLI

--- src/TaskCLI.java
+++ src/TaskCLI.java
@@ -1,6 +1,28 @@
 public class TaskCLI {
-    public static void main(String[] args) {
-        System.out.println("Welcome to TaskCLI!");
-        
-    }
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("Task Cli App - No commands provided. Use 'help' to see available commands.");
+			return;
+		}
+
+		String commands = args[0];
+
+		switch (commands) {
+			case "add":
+				System.out.println("Adding a new task...");
+				break;
+			case "list":
+				System.out.println("Listing all tasks...");
+				break;
+			case "remove":
+				System.out.println("Removing a task...");
+				break;
+			case "help":
+				System.out.println("Available commands: add, list, remove, help");
+				break;
+			default:
+				System.out.println("Unknown command. Use 'help' to see available commands.");
+				break;
+			}
+	}
 }
